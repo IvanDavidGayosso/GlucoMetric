3a4856eb4d6aede22f197742ebcf82e54804b914
IvanDavidGayosso <31744431+IvanDavidGayosso@users.noreply.github.com>
2018-04-17 13:03:55 -0500

    Update Splash.java
    Codificación

--- app/src/main/java/com/example/ivan/glucometric/Splash.java
+++ app/src/main/java/com/example/ivan/glucometric/Splash.java
@@ -1,3 +1,4 @@
+
 package com.example.ivan.glucometric;
 
 import android.app.Fragment;
@@ -8,15 +9,18 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Clase de Splash el cual solo creara al Fragment Inicio de sesion
 public class Splash extends AppCompatActivity {
     Fragment  inicio_sesion;
 
 
 
+    
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_splash);
 
+            //Genera una plantalla en un determinado tiempo (2.5 seg) y posteriormente enviara a la aplicacion a la Interfaz (InicioSesion)
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
